c8c090cf7c4a28578f737b20c5deb5e6cc7abc76
oeoeoyee <polly931621@gmail.com>
2025-08-10 12:06:51 +0800

    [feat]let todoCard component work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import Image from "next/image";
 import { useState } from "react";
-import TodoCard from "@/component/index/todoCard";
+import TodoCard from "@/component/index/TodoCard";
 // 1. 先跑一個陣列含有需要數量的undifed
 // 2. 在生成html那邊，跑map做陣列
 // const todoList = [{}]
@@ -9,10 +11,14 @@ import TodoCard from "@/component/index/todoCard";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"> 
-      <main>
-        <TodoCard></TodoCard>
-      </main>       
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <div> 123</div>
+      {/* <main> */}
+        <TodoCard
+          cardDetail={{title:'今日任務', description:'今天沒事但還是想做點什麼', type:['程式','日常','運動']}}
+          subTask={['好初看漫畫','順便去板新站URLiving','圖書館做瑣事']}
+        />
+      {/* </main> */}
+      {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -58,7 +64,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
--- src/component/index/TodoCard.tsx
+++ src/component/index/TodoCard.tsx
@@ -1,29 +1,47 @@
-'use client'
 import { useState  } from 'react';
 
-export default function todoCard() {
+type TodoCardProps = {
+  cardDetail: {
+    title: string;
+    description: string;
+    type: string[];
+  };
+  subTask: string[];
+};
+
+// 組件接收參數
+export default function TodoCard({cardDetail,subTask}:TodoCardProps) {
+  // let cardDetail = props.cardDetail
+  // let subTask = props.subTask  
   const [count, setCount] = useState(0)
   const increment = () => setCount(count + 1)
 
   return (
-    <div>
+    <div 
+      className="cardSection"
+      >
         <button onClick={increment}>Count: {count}</button>
         <div>
-            <h3>卡片標題</h3>
-            <p>任務說明</p>
+            <h3 className='text-[18px] font-[700]'>{cardDetail?.title ?? '標題錯誤'}</h3>
+            <p className='text-[14px] text-[#1D2939] font-[600] opacity-60'>{cardDetail.description ?? ''}</p>
         </div>
         <div>
-            <span>Sub task</span>
+            <span className='text-[13px]'>Sub task</span>
             <ul>
-                <li>
-                {/* checkbox組件 */}
-                <input type="checkbox" name="" id="" />
-                <span>事項1</span>
+              {subTask.map((taskItem, index) => (
+                <li key={index}>
+                  <input type="checkbox" />
+                  <span>{taskItem}</span>
                 </li>
-                <li>
-                <input type="checkbox" name="" id="" />
-                <span>事項2</span>
-              </li>
+              ))}
+                {/* 
+                這是隱式回傳，不需要有return也不用()、{}
+                subTask.map(taskItem=>
+                  <li>
+                  <input type="checkbox" name="" id="" />
+                  <span  className='text-[14px]'>{taskItem}</span>
+                  </li>
+                )*/}
             </ul>
         </div>
         <div>
